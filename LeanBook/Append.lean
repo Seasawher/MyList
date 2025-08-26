fc602d5deb150b7477df6ce0fd0dbafd77d09d8b
Seasawher <tellagoodstory@icloud.com>
2025-08-26 20:22:48 +0900

    `ac_rfl`を使用できるようにする

--- LeanBook/Append.lean
+++ LeanBook/Append.lean
@@ -1,11 +1,9 @@
 import LeanBook.MyList --#
 /- # Appendの結合性 -/
 
-namespace MyList
-
 variable {α : Type}
 
-protected def append (l₁ l₂ : MyList α) : MyList α :=
+protected def MyList.append (l₁ l₂ : MyList α) : MyList α :=
   match l₁ with
   | ⟦⟧ => l₂
   | head :: tail => head :: (MyList.append tail l₂)
@@ -15,25 +13,32 @@ instance : Append (MyList α) where
   append := MyList.append
 
 @[simp, grind _=_]
-theorem cons_append (head : α) (tail l : MyList α) :
+theorem MyList.cons_append (head : α) (tail l : MyList α) :
     (head :: tail) ++ l = head :: (tail ++ l) := by
   rfl
 
-theorem append_cons (head : α) (tail l : MyList α) :
+theorem MyList.append_cons (head : α) (tail l : MyList α) :
     l ++ head :: tail = l ++ (⟦head⟧ ++ tail) := by
   rfl
 
 @[simp, grind]
-theorem nil_append (l : MyList α) : ⟦⟧ ++ l = l := by
+theorem MyList.nil_append (l : MyList α) : ⟦⟧ ++ l = l := by
   rfl
 
 @[simp, grind]
-theorem append_nil (l : MyList α) : l ++ ⟦⟧ = l := by
+theorem MyList.append_nil (l : MyList α) : l ++ ⟦⟧ = l := by
   induction l with grind
 
-@[grind _=_]
-theorem append_assoc (l₁ l₂ l₃ : MyList α) :
+-- @[grind _=_]
+theorem MyList.append_assoc (l₁ l₂ l₃ : MyList α) :
     (l₁ ++ l₂) ++ l₃ = l₁ ++ (l₂ ++ l₃) := by
   induction l₁ with grind
 
-end MyList
+/- ## 結合性を再利用可能にする -/
+
+-- 結合的であることを`ac_rfl`から再利用可能にする
+instance {α : Type} : Std.Associative (α := MyList α) (· ++ ·) where
+  assoc := MyList.append_assoc
+
+example (l₁ l₂ l₃ : MyList α) : l₁ ++ (l₂ ++ l₃) = (l₁ ++ l₂) ++ l₃ := by
+  ac_rfl
